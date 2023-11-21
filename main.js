ef9ac2ae862f2a40f2b0c0c847c8a91a492e7f5d
Timothy Coleman <Timbo97@yahoo.com>
2023-11-21 14:42:38 -0500

    before checking with hunter

--- main.js
+++ main.js
@@ -1,47 +1,95 @@
 console.log("Script attached");
 /*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
 
-// Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
+var node1 = document.getElementById("node1");
+node1.innerText = "I used the getElementById('node1') method to access this";
 
-// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
+var node2 = document.getElementsByClassName("node2")[0];
+node2.innerText = "I used the getElementByClassName('node2') method to access this";
 
-// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+var h3Nodes = document.getElementsByTagName("h3");
+for (var i = 0; i < h3Nodes.length; i++) {
+  h3Nodes[i].innerText = "I used the getElementByTagName('h3') method to access all of these";
+}
 
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
-// TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
+var newParagraph = document.createElement("p");
+newParagraph.innerText = "This node was created using the createElement() method";
 
-// TODO: Append the created node to the parent node using the element.appendChild() method
 
-// TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
+var parentContainer = document.getElementById("parent");
+parentContainer.appendChild(newParagraph);
 
-// BONUS: Add a link href to the <a>
 
-// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+var newLink = document.createElement("a");
+newLink.innerText = "I am a <a> tag";
+
+
+newLink.href = "https://www.example.com";
+
+
+parentContainer.insertBefore(newLink, newParagraph);
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
-// TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
+var childNode = document.getElementById("N1");
+var newChildNode = document.createElement("p");
+newChildNode.innerText = "New Child Node";
+childNode.parentNode.replaceChild(newChildNode, childNode);
 
-// TODO: Remove the "New Child Node"
+setTimeout(function () {
+  newChildNode.parentNode.removeChild(newChildNode);
+}, 2000);
 
 /*----------- Exercise #4: LIST ITEMS ----------- */
-// Use the following array of values to generate a list on the DOM
 
 let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish", "grapes", "honey", "ice bag", "juice (any kind)" ];
 
+var ulElement = document.createElement("ul");
 
-// TODO: Create an unordered list element
 
-// TODO: Iterate over the array values, and create a list item element for each
+for (var i = 0; i < list.length; i++) {
+  var liElement = document.createElement("li");
+  liElement.innerText = list[i];
+  ulElement.appendChild(liElement);
+}
 
-// TODO: Append the new list items to the unordered list element
-
-// TODO: Append the unordered list to the `div#container` under exercise 4 
+var container4 = document.querySelector(".exercise-container.exercise4 #container");
+container4.appendChild(ulElement);
 
 /*----------- Exercise #5: DOM EVENTS --------------*/
 
-// TODO: write a function called "show" which creates a new div with an alerting message to the user with this message
-// -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
-// This div should be a 'modal' that covers the main content on the screen
-// BONUS: The modal popup should be able to be closed. Refactor for this functionality
+function show() {
+  var modalDiv = document.createElement("div");
+  modalDiv.style.position = "fixed";
+  modalDiv.style.top = "0";
+  modalDiv.style.left = "0";
+  modalDiv.style.width = "100%";
+  modalDiv.style.height = "100%";
+  modalDiv.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  modalDiv.style.color = "white";
+  modalDiv.style.display = "flex";
+  modalDiv.style.justifyContent = "center";
+  modalDiv.style.alignItems = "center";
+
+  var message = document.createElement("p");
+  message.innerText = "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user";
+
+  var closeButton = document.createElement("button");
+  closeButton.innerText = "Close";
+  closeButton.addEventListener("click", function() {
+    modalDiv.style.display = "none";
+  });
+
+  modalDiv.appendChild(message);
+  modalDiv.appendChild(closeButton);
+
+  document.body.appendChild(modalDiv);
+
+  
+  alert("Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user");
+}
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", show);
